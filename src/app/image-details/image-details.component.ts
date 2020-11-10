734f58c0edfb89a8daec1e91a68ee5eeb696b436
svasquez <svasquez@controldiseno.com>
2020-11-09 20:48:58 -0500

    Cambios de code smells

--- src/app/image-details/image-details.component.ts
+++ src/app/image-details/image-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../image.service';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './image-details.component.html',
